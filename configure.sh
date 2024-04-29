392595f56f56059bf9a1d53c0903b9a8966cf320
C. Titus Brown <titus@idyll.org>
2024-04-28 21:19:14 -0700

    ssh key comment out

--- configure.sh
+++ configure.sh
@@ -2,6 +2,11 @@
 set -x
 set -e
 
+. /opt/miniconda3/etc/profile.d/conda.sh
+
+# add my ssh key
+echo ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHFz3WLVqV+0md4OkZi/S0a79cOO7Ax8S4Dledp832JhMQ0GJ0ZlmEnWZrIv83KnRexpAEi5w6H1aSackGjucgQ= t@TitusMatsalmoth.attlocal.net >> ~/.ssh/authorized_keys
+
 mkdir ~/data/
 
 cd ~/data/
