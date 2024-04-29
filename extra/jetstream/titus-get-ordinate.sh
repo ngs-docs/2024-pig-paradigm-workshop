2498befcd5c70fea69e20156e694dec9f47aa2d6
C. Titus Brown <titus@idyll.org>
2024-04-29 11:10:28 -0700

    add actual config scripts for jetstream instances

---
+++ extra/jetstream/titus-get-ordinate.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+mkdir ~/scripts
+cd ~/scripts
+curl -JL -o ordinate.py https://raw.githubusercontent.com/ngs-docs/2024-pig-paradigm-workshop/main/scripts/ordinate.py
+chmod +x ordinate.py
